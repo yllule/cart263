e92022a82ab5039a966bac048d2da35d9e2c4127
Catherine Z <catherinezaloshnja@gmail.com>
2024-01-17 21:45:14 -0500

    Finished 2.3 first class functions video

--- experiments/2-Review/template-p5-project/js/script.js
+++ experiments/2-Review/template-p5-project/js/script.js
@@ -31,27 +31,10 @@ Description of draw()
 function draw() {
     background(0);
 
-    let config = {
-        x: 250,
-        y: 250,
-        width: 200,
-        height: 200,
-        fillColor: {
-            r: 255,
-            g: 255,
-            b: 0
-        },
-        mode: CENTER
-    };
-
-    drawFancyRect(config);
-
 }
 
-function drawFancyRect( { x, y, width, height, fillColor, mode }) {
-    push();
-    fill(fillColor.r, fillColor.g, fillColor.b);
-    rectMode(mode);
-    rect(x, y, width, height);
-    pop();
-}
+let hello = function () {
+    alert('Hello');
+};
+
+setTimeout(hello, 5000);
