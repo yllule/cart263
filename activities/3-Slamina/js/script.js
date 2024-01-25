e3f4defa2cf7e08d3dd19e17d45355fae7756f6c
yllule <catherinezaloshnja@gmail.com>
2024-01-25 16:39:36 -0500

    Finished Slamina activity

--- activities/3-Slamina/js/script.js
+++ activities/3-Slamina/js/script.js
@@ -1,18 +1,159 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Slamina
+Catherine Zaloshnja
 */
 
 "use strict";
+
+const animals = [
+    "aardvark",
+    "alligator",
+    "alpaca",
+    "antelope",
+    "ape",
+    "armadillo",
+    "baboon",
+    "badger",
+    "bat",
+    "bear",
+    "beaver",
+    "bison",
+    "boar",
+    "buffalo",
+    "bull",
+    "camel",
+    "canary",
+    "capybara",
+    "cat",
+    "chameleon",
+    "cheetah",
+    "chimpanzee",
+    "chinchilla",
+    "chipmunk",
+    "cougar",
+    "cow",
+    "coyote",
+    "crocodile",
+    "crow",
+    "deer",
+    "dingo",
+    "dog",
+    "donkey",
+    "dromedary",
+    "elephant",
+    "elk",
+    "ewe",
+    "ferret",
+    "finch",
+    "fish",
+    "fox",
+    "frog",
+    "gazelle",
+    "gila monster",
+    "giraffe",
+    "gnu",
+    "goat",
+    "gopher",
+    "gorilla",
+    "grizzly bear",
+    "ground hog",
+    "guinea pig",
+    "hamster",
+    "hedgehog",
+    "hippopotamus",
+    "hog",
+    "horse",
+    "hyena",
+    "ibex",
+    "iguana",
+    "impala",
+    "jackal",
+    "jaguar",
+    "kangaroo",
+    "koala",
+    "lamb",
+    "lemur",
+    "leopard",
+    "lion",
+    "lizard",
+    "llama",
+    "lynx",
+    "mandrill",
+    "marmoset",
+    "mink",
+    "mole",
+    "mongoose",
+    "monkey",
+    "moose",
+    "mountain goat",
+    "mouse",
+    "mule",
+    "muskrat",
+    "mustang",
+    "mynah bird",
+    "newt",
+    "ocelot",
+    "opossum",
+    "orangutan",
+    "oryx",
+    "otter",
+    "ox",
+    "panda",
+    "panther",
+    "parakeet",
+    "parrot",
+    "pig",
+    "platypus",
+    "polar bear",
+    "porcupine",
+    "porpoise",
+    "prairie dog",
+    "puma",
+    "rabbit",
+    "raccoon",
+    "ram",
+    "rat",
+    "reindeer",
+    "reptile",
+    "rhinoceros",
+    "salamander",
+    "seal",
+    "sheep",
+    "shrew",
+    "silver fox",
+    "skunk",
+    "sloth",
+    "snake",
+    "squirrel",
+    "tapir",
+    "tiger",
+    "toad",
+    "turtle",
+    "walrus",
+    "warthog",
+    "weasel",
+    "whale",
+    "wildcat",
+    "wolf",
+    "wolverine",
+    "wombat",
+    "woodchuck",
+    "yak",
+    "zebra"
+];
+
+let currentAnimal = ``;
+let currentAnswer = ``;
+
+const speechSynthesizer = new p5.Speech();
+const speechRecognizer = new p5.SpeechRec();
 
 
 /**
 Description of preload
 */
 function preload() {
+
 
 }
 
@@ -21,7 +162,10 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+    createCanvas(500, 500);
+    speechRecognizer.continuous = true;
+    speechRecognizer.onResult = handleSpeechInput;
+    speechRecognizer.start();
 }
 
 
@@ -29,5 +173,48 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(0);
 
+    if(currentAnswer === currentAnimal) {
+        fill(123, 231, 112);
+    }
+    else {
+        fill(200, 50, 50);
+    }
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentAnswer, width/2, height/2);
+    // text(currentAnimal, width/2, height/2);
+}
+
+function mousePressed() {
+    currentAnimal = random(animals);
+    let reverseAnimal = reverseString(currentAnimal);
+    speechSynthesizer.speak(reverseAnimal);
+
+}
+
+function handleSpeechInput() {
+    let guessedAnimal = `What???`;
+    if (speechRecognizer.resultValue) {
+        let lowerCaseResult = speechRecognizer.resultString.toLowerCase();
+        let parts = lowerCaseResult.split(`i think it is `);
+        if (parts.length > 1) {
+            guessedAnimal = parts[1];
+        }
+    }
+
+    currentAnswer = guessedAnimal;
+    console.log(currentAnswer);
+}
+
+function reverseString(string) {
+    // Split the string into an array of characters
+    let characters = string.split(``);
+    // Reverse the array of characters
+    let reverseCharacters = characters.reverse();
+    // Join the array of characters back into a string
+    let result = reverseCharacters.join(``);
+    // Return the result
+    return result;
 }
