36035ee8b679b665560258a64f84914c4378298a
Catherine Z <catherinezaloshnja@gmail.com>
2024-02-20 23:19:33 -0500

    Added basic code + Played around with bubble popper activity code

--- Ai_Jam/template-p5-project/js/script.js
+++ Ai_Jam/template-p5-project/js/script.js
@@ -14,11 +14,21 @@ Play rock paper scissors with the computer
 
 "use strict";
 
+let video = undefined;
+//handpose model
+let handpose = undefined;
+//current set of predictions
+let predictions = [];
+
+//temporary img
+let clownImg;
+
 
 /**
 Description of preload
 */
 function preload() {
+    clownImg = loadImage('assets/images/clown.png');
 
 }
 
@@ -27,7 +37,24 @@ function preload() {
 Description of setup
 */
 function setup() {
+    createCanvas(1000,1000);
 
+    //access the user's webcam
+    video = createCapture(VIDEO);
+    video.hide();
+
+    //load the handpose model
+    handpose = ml5.handpose(video, {
+        flipHorizontal: true
+    }, function () {
+        console.log('Model loaded');
+    });
+
+    //listen for predictions
+    handpose.on(`predict`, function (results) {
+        console.log(results);
+        predictions = results;
+    })
 }
 
 
@@ -35,5 +62,19 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(0);
+
+    if (predictions.length > 0) {
+        let hand = predictions[0];
+        let index = hand.annotations.indexFinger;
+        let tip = index[3];
+        let base = index[0];
+        let tipX = tip[0];
+        let tipY = tip[1];
+        let baseX = base[0];
+        let baseY = base[1];
+        image(clownImg, tipX, tipY);
+        image(clownImg, baseX, baseY);
+    }
 
 }
