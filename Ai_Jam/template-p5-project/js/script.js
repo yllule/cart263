12d32e168b5dbfb7d8e53305c72603781b6caee0
yllule <catherinezaloshnja@gmail.com>
2024-02-22 16:17:51 -0500

    Made the circle draggable only when cursor is on top of it

--- Ai_Jam/template-p5-project/js/script.js
+++ Ai_Jam/template-p5-project/js/script.js
@@ -1,5 +1,5 @@
 /**
-Duck customization (hard mode)
+Birb boudoir
 Catherine Zaloshnja
 */
 
@@ -9,6 +9,18 @@ Catherine Zaloshnja
 //3- make it possible to change "settings" (ex:filters, colors) by pointing with index
 //3- make ui/assets
 //4- add states (tutorial/customization/confirm)
+
+//asset list
+//-5 bg images (simple)
+//-base birb body
+//-4 different birb colorings
+//-3 hats
+//-2 bows
+//-2 scarfs
+//-2 necklace
+//-3 shoes
+//-2 glasses
+//-5 frames/deco (simple)
 
 
 "use strict";
@@ -20,24 +32,30 @@ let handpose = undefined;
 let predictions = [];
 
 //temporary img
-let scissorsImg;
+// let scissorsImg;
 
 let cursor = {
     x: undefined,
-    y: undefined
-}
+    y: undefined,
+    size: 50
+};
 
 let test = {
     x: undefined,
-    y: undefined
-}
+    y: undefined,
+    size: 200
+};
+
+//an array to store all the draggable items   
+let items = [
+]
 
 
 /**
 Description of preload
 */
 function preload() {
-    scissorsImg = loadImage('assets/images/scissors.png');
+    // scissorsImg = loadImage('assets/images/scissors.png');
 
 }
 
@@ -65,8 +83,8 @@ function setup() {
         predictions = results;
     })
 
-    test.x = 500;
-    test.y = 500;
+    test.x = 200;
+    test.y = 200;
 }
 
 
@@ -74,37 +92,45 @@ function setup() {
 Description of draw()
 */
 function draw() {
-    background(255);
+    background(100);
+
+    fill(0);
+    circle(test.x, test.y, test.size);
 
     if (predictions.length > 0) {
         let hand = predictions[0];
+        //variables for the index finger
         let index = hand.annotations.indexFinger;
         let indexTip = index[3];
         let indexTipX = indexTip[0];
         let indexTipY = indexTip[1];
+        //variables for the thumb
         let thumb = hand.annotations.thumb;
         let thumbTip = thumb[3];
         let thumbTipX = thumbTip[0];
         let thumbTipY = thumbTip[1];
+        //distance btw index and thumb and distance between thumb and object
+        //(used to check if the thumb and index are touching over the object)
         let d = dist(indexTipX, indexTipY, thumbTipX, thumbTipY);
+        let d2 = dist(cursor.x, cursor.y, test.x, test.y);
+
+        //setting the coordinates of the index cursor
         cursor.x = indexTipX;
         cursor.y = indexTipY;
-        if (d < 25) {
+        
+        if (d < 25 && d2 < cursor.size + test.size/2) {
+            console.log('touching tips');
             test.x = indexTipX;
             test.y = indexTipY;
         }
         push();
         noStroke();
         fill(255, 0, 0);
-        circle(cursor.x, cursor.y, 50);
+        circle(cursor.x, cursor.y, cursor.size);
         pop();
 
         fill(0, 0, 255);
         circle(thumbTipX, thumbTipY, 50);
     }
-
-
-    fill(0);
-    circle(test.x, test.y, 200);
 
 }
