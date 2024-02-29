910c590e4a08662bae04b78aa2826c22dd9ab656
Catherine Z <catherinezaloshnja@gmail.com>
2024-02-29 15:05:02 -0500

    Phaser Physics and Movement done

--- experiments/5-Game_Engine/js/Play.js
+++ experiments/5-Game_Engine/js/Play.js
@@ -7,12 +7,30 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        this.clown = this.add.image(100, 100, 'clown');
+        this.clown = this.physics.add.image(100, 100, 'clown');
         this.clown.setTint(0xdd3333);
+        // this.clown.setVelocityX(100);
+        this.clown.setCollideWorldBounds(true);
 
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
+        this.clown.setVelocity(0);
+
+        if (this.cursors.left.isDown) {
+            this.clown.setVelocityX(-300);
+        }
+        else if (this.cursors.right.isDown) {
+            this.clown.setVelocityX(300);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.clown.setVelocityY(-300);
+        }
+        else if (this.cursors.down.isDown) {
+            this.clown.setVelocityY(300);
+        }
 
     }
 }
