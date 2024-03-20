4527cad4a30f845a01fde709074c8251cdf464e5
Catherine Z <catherinezaloshnja@gmail.com>
2024-03-20 12:56:44 -0400

    Made the items collectable

--- Game_Engine_Jam/js/Play.js
+++ Game_Engine_Jam/js/Play.js
@@ -25,31 +25,55 @@ class Play extends Phaser.Scene {
         //creating the minimap (first 2 variables are x+y positions)
         this.minimap = this.cameras.add(0, 0, 0, 0).setZoom(0.04).setName('mini');
 
+            this.book = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'book').setInteractive(),
+            this.diamond = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'diamond').setInteractive(),
+            this.fishing = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'fishing').setInteractive(),
+            this.flower = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'flower').setInteractive(),
+            this.fossil = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'fossil').setInteractive(),
+            this.ghost = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'ghost').setInteractive(),
+            this.glove = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'glove').setInteractive(),
+            this.gun = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'gun').setInteractive(),
+            this.jar = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'jar').setInteractive(),
+            this.mushroom = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'mushroom').setInteractive(),
+            this.necklace = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'necklace').setInteractive(),
+            this.note = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'note').setInteractive(),
+            this.photo = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'photo').setInteractive(),
+            this.potion = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'potion').setInteractive(),
+            this.rock = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'rock').setInteractive(),
+            this.shard = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'shard').setInteractive(),
+            this.sheep = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'sheep').setInteractive(),
+            this.skull = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'skull').setInteractive(),
+            this.sword = this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'sword').setInteractive()
+
+        //when items are clicked on they disappear
+        this.book.on("pointerdown", collect)
+        this.diamond.on("pointerdown", collect)
+        this.fishing.on("pointerdown", collect)
+        this.flower.on("pointerdown", collect)
+        this.fossil.on("pointerdown", collect)
+        this.ghost.on("pointerdown", collect)
+        this.glove.on("pointerdown", collect)
+        this.gun.on("pointerdown", collect)
+        this.jar.on("pointerdown", collect)
+        this.mushroom.on("pointerdown", collect)
+        this.necklace.on("pointerdown", collect)
+        this.note.on("pointerdown", collect)
+        this.photo.on("pointerdown", collect)
+        this.potion.on("pointerdown", collect)
+        this.rock.on("pointerdown", collect)
+        this.shard.on("pointerdown", collect)
+        this.sheep.on("pointerdown", collect)
+        this.skull.on("pointerdown", collect)
+        this.sword.on("pointerdown", collect)
+
+        //makes the items disappear (alpha = 0)
+        function collect() {
+            this.setAlpha(0);
+        }
+        
         //create the avatar
         this.avatar = this.physics.add.sprite(this.width/2, 0, 'avatar');
         this.avatar.setCollideWorldBounds(true);
-
-        this.trinkets = [
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'book'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'diamond'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'fishing'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'flower'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'fossil'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'ghost'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'glove'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'gun'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'jar'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'mushroom'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'necklace'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'note'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'photo'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'potion'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'rock'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'shard'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'sheep'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'skull'),
-            this.physics.add.sprite(Math.random() * 5000, Math.random() * 5000, 'sword')
-        ];
 
         //make the camera follow the avatar
         this.cameras.main.startFollow(this.avatar, true);
@@ -63,7 +87,6 @@ class Play extends Phaser.Scene {
         //light follows the avatar
         this.light.x = this.avatar.x;
         this.light.y = this.avatar.y;
-    
     }
 
   handleInput() {
