c7579bb3eaa07dc55f025bc97f9522da56914e60
yllule <catherinezaloshnja@gmail.com>
2024-03-21 16:36:18 -0400

    Finished dom overview

--- experiments/6-Webpage/js/script.js
+++ experiments/6-Webpage/js/script.js
@@ -1,11 +1,58 @@
-/**
-Title of Project
-Author Name
+let mainHeading = document.getElementById('main-heading');
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+//.style accesses css properties, change css properties in js
+mainHeading.style['color'] = '#339966'; //can be written either like this or like v
+mainHeading.style.fontSize = '4rem';
+mainHeading.style.fontFamily = 'Courier, monospace';
+mainHeading.style.backgroundColor = 'red';
 
-"use strict";
+let wellSection = document.getElementById('well-section');
+wellSection.style['color'] = '#445566';
 
-// Code goes here
+let pronoun = document.getElementById('pronoun');
+
+//access html properties
+pronoun.innerHTML = '<strong>you</strong>';
+
+if (pronoun.innerText === 'we') {
+    pronoun.innerText = 'you';
+}
+
+let img = document.getElementById('clown-image');
+
+img.setAttribute('src', 'http://loremflickr.com/320/240/clown');
+
+// let headers = document.getElementsByClassName('header');
+
+// for (let i = 0; i < headers.length; i++) {
+//     headers[i].style['color'] = '#ff0000';
+// }
+
+let headers = document.querySelectorAll('.header');
+
+for (let i = 0; i < headers.length; i++) {
+    headers[i].style['color'] = '#ff0000';
+}
+
+// let h2s = document.getElementsByTagName('h2');
+
+// for (let i = 0; i < h2s.length; i++) {
+//     h2s[i].style['color'] = '#ff0000';
+// }
+
+
+//add elements to a page
+let newP = document.createElement('p');
+
+newP.innerText = "Gosh, I sure do like clowns."; //to make this show up on page, add it to clown-section
+
+let clownSection = document.getElementById('clown-section');
+
+clownSection.appendChild(newP);
+
+//remove elements of a page
+
+// let mainHeading = document.getElementById('main-heading');
+
+// //parent element = html body
+// mainHeading.parentElement.removeChild(mainHeading);
