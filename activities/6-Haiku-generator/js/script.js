e0fa6bfada24058e240f7a29220a1ccf568522a8
Catherine Z <catherinezaloshnja@gmail.com>
2024-03-25 23:40:39 -0400

    Finished haiku generator activity

--- activities/6-Haiku-generator/js/script.js
+++ activities/6-Haiku-generator/js/script.js
@@ -1,11 +1,80 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Haiku generator
+Catherine Zaloshnja
 */
 
 "use strict";
 
-// Code goes here
+let fiveSyllableLines = [
+    'O, to be a tree',
+    'The cat does not know',
+    'We are all forests',
+    'You have done your best',
+    'They are all gone now'
+];
+
+let sevenSyllableLines = [
+    'Say the things left unsaid',
+    `Never believe the wind's lies`,
+    'The autumn stretches its legs',
+    'Nothing can satisfy you',
+    'They will not come back again'
+];
+
+let line1 = random(fiveSyllableLines);
+let line2 = random(sevenSyllableLines);
+let line3 = random(fiveSyllableLines);
+
+let line1P = document.getElementById('line-1');
+let line2P = document.getElementById('line-2');
+let line3P = document.getElementById('line-3');
+
+line1P.innerText = line1;
+line2P.innerText = line2;
+line3P.innerText = line3;
+
+line1P.addEventListener('click', lineClicked);
+line2P.addEventListener('click', lineClicked);
+line3P.addEventListener('click', lineClicked);
+
+function lineClicked(event) {
+    fadeOut(event.target, 1);
+}
+
+function fadeOut(element, opacity) {
+    opacity -= 0.01;
+    element.style['opacity'] = opacity;
+    if (opacity > 0) {
+        requestAnimationFrame(function() {
+            fadeOut(element, opacity);
+        });
+    }
+    else {
+        setNewLine(element);
+        fadeIn(element, opacity);
+    }
+}
+
+function fadeIn(element, opacity) {
+    opacity += 0.01;
+    element.style['opacity'] = opacity;
+    if (opacity < 1) {
+        requestAnimationFrame(function() {
+            fadeIn(element, opacity);
+        });
+    }
+}
+
+function setNewLine(element) {
+    if (element === line1P || element === line3P) {
+        element.innerText = random(fiveSyllableLines);
+    }
+    else if (element === line2P) {
+        element.innerText = random(sevenSyllableLines);
+    }
+}
+
+function random(array) {
+    let index = Math.floor(Math.random() * array.length);
+    return array[index];
+}
