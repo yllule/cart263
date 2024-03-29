480bf85bca4c9a82c7c51535463df73d53bc7973
Catherine Z <catherinezaloshnja@gmail.com>
2024-03-29 12:46:17 -0400

    Named proj

--- Final_Proj_Prototype/js/script.js
+++ Final_Proj_Prototype/js/script.js
@@ -1,9 +1,6 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Catherine Zaloshnja
+Final Project
 */
 
 "use strict";
