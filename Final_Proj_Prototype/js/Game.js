e35da7b2b83ef833823ee12187d9841c944bc5d0
Catherine Z <catherinezaloshnja@gmail.com>
2024-04-01 23:45:11 -0400

    Made it possible to switch between scenes

--- Final_Proj_Prototype/js/Game.js
+++ Final_Proj_Prototype/js/Game.js
@@ -7,7 +7,27 @@ class Game extends Phaser.Scene {
 
     create() {
 
+        this.input.once(Phaser.Input.Events.POINTER_DOWN, function ()
+        {
 
+            this.scene.switch('play');
+
+        }, this);
+
+
+        this.events.on(Phaser.Scenes.Events.WAKE, function ()
+        {
+            this.wake(this.input, this.scene);
+        }, this);
+
+    }
+
+    wake (input, scene)
+    {
+        input.once(Phaser.Input.Events.POINTER_DOWN, event =>
+        {
+            scene.switch('sceneA');
+        }, this);
     }
 
     update() {
--- Final_Proj_Prototype/js/Play.js
+++ Final_Proj_Prototype/js/Play.js
@@ -9,10 +9,25 @@ class Play extends Phaser.Scene {
         this.test = this.add.image(400, 400, 'test');
         this.test.setInteractive();
 
-        //when you click on test img, starts new scene called game
-        this.test.on('pointerdown', () => this.scene.start('game'))
+        //when you click on test img, switches to scene called game
+        this.test.on('pointerdown', () => this.scene.switch('game'))
+
+        this.events.on(Phaser.Scenes.Events.WAKE, function ()
+        {
+            this.wake(this.input, this.scene);
+        }, this);
 
 
+    }
+
+    //makes it possible to 'save' where previous state was when switching states
+    wake (input, scene)
+    {
+        input.once(Phaser.Input.Events.POINTER_DOWN, () =>
+        {
+            scene.switch('game');
+
+        }, this);
     }
 
     update() {
